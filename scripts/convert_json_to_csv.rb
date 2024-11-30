362cc184d40a9056ad5875e45993fbbce0b50d69
Nataliia Makarenko <73568587+NVMakarenko@users.noreply.github.com>
2024-11-30 20:52:08 +0200

    Update convert_json_to_csv.rb

--- scripts/convert_json_to_csv.rb
+++ scripts/convert_json_to_csv.rb
@@ -1,19 +1,17 @@
-require 'json'
-require 'csv'
+require "json"
+require "csv"
 
-input_file = '.unlighthouse/dist/ci-result.json'
-output_file = '.unlighthouse/dist/ci-result.csv'
-
-begin
-  json_data = JSON.parse(File.read(input_file))
-  CSV.open(output_file, 'w', write_headers: true, headers: json_data.first.keys) do |csv|
-    json_data.each do |row|
-      csv << row.values
+class ConvertJsonToCsv
+  def self.perform(input_file, output_file)
+    json_data = JSON.parse(File.read(input_file))
+    CSV.open(output_file, "w", write_headers: true, headers: json_data.first.keys) do |csv|
+      json_data.each { |row| csv << row.values }
     end
   end
-  puts "CSV file created successfully: #{output_file}"
-rescue Errno::ENOENT
-  puts "File not found: #{input_file}"
-rescue JSON::ParserError => e
-  puts "Error parsing JSON: #{e.message}"
+end
+
+if __FILE__ == $0
+  input_file  = ARGV[0]
+  output_file = ARGV[1]
+  ConvertJsonToCsv.perform(input_file, output_file)
 end
